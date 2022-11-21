bf86c8e1e967eaf9451ae44cfb021dbd3caf23e9
Mariia <mariia.kushnir.personal@gmail.com>
2022-11-21 22:30:36 +0200

    project created
    added the files to the project, setup the yarn

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header"></header>
+        <Dictionary defaultKeyword="sunset" />
+        <footer>
+          This project coded by Mariia Kushnir and is{" "}
+          <a
+            href="https://github.com/MariiaChitra/react-dictionary-search-engine"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
